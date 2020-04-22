1daa9c34669e71b69451058e24c38d8a32157165
Johan Uddståhl <baktakt@gmail.com>
2020-04-22 13:31:22 +0200

    Adding callback function

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@ import App from './app'
 import * as bodyParser from 'body-parser'
 import loggerMiddleware from './middleware/logger'
 import CaptureController from './controllers/capture/Capture.controller'
+import CallbackController from './controllers/callback/Callback.controller'
 
 const app = new App({
   port: process.env.PORT || 3000,
   controllers: [
-    new CaptureController()
+    new CaptureController(),
+    new CallbackController()
+
   ],
   middleWares: [
     bodyParser.json(),
