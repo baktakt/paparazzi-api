0b1b8f7b4d3739d4b5d1f838bd34c0df8f6b5f84
Johan Uddståhl <baktakt@gmail.com>
2020-04-22 13:42:34 +0200

    Removed callback

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,11 @@ import App from './app'
 import * as bodyParser from 'body-parser'
 import loggerMiddleware from './middleware/logger'
 import CaptureController from './controllers/capture/Capture.controller'
-import CallbackController from './controllers/callback/Callback.controller'
 
 const app = new App({
   port: process.env.PORT || 3000,
   controllers: [
-    new CaptureController(),
-    new CallbackController()
-
+    new CaptureController()
   ],
   middleWares: [
     bodyParser.json(),
